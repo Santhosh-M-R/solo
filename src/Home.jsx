052ca0eb5555f111aeec5a348516c55e005c8eb3
Santhosh M R <santhoshmr@Santhoshs-MacBook-Air.local>
2024-03-09 15:04:57 +0530

    text changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -109,7 +109,7 @@ const Home = () => {
           </p>
           <p>
             The thing here is when we did this then we will get to know about
-            patience, emotions and how to be cool in dificult times, then slowly
+            patience, emotions and how to be cool in difficult times, then slowly
             increased lots in options trading and make it consistency.
           </p>
         </div>
